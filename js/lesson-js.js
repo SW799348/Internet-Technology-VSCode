e22d13ce8c63a795288df6d9a1ecc2d67b268024
sw799348 <sw799348@gmail.com>
2024-03-30 14:52:18 +0530

    javasript started

---
+++ js/lesson-js.js
@@ -0,0 +1,4 @@
+console.log("add:"+(10+20));
+console.log("sub:"+(10-20));
+console.log("div:",(10/20));
+console.log("multi:"+(10*20));
---
+++ js/lesson-variable.js
@@ -0,0 +1,18 @@
+//keywords in variables defining in javasript
+//var.let.const
+
+let firstName = "kamal"
+let lastName = "perera"
+
+const age = 30;
+var salary = 50000.00
+
+console.log(firstName + " " + lastName);
+
+
+//IMPORTANT
+//1.can not be a reserved word
+//2.variable name should be meaningfull
+//3.can not start with numbers
+//4.can not contain spaces or hypens(-)
+//5.case sensitive
