ede957659114659f17e6d2ad9a1d8e6fb60983ee
sw799348 <sw799348@gmail.com>
2024-04-06 09:40:50 +0530

    js started

---
+++ js/js-arrays.js
@@ -0,0 +1,18 @@
+let fruits = []
+console.log('tyoe of fruits : ', typeof fruits);
+console.log('fruits array :', fruits);
+console.log('fruits array length :', fruits.length);
+
+fruits = ['apple', 'banana', 'mango']
+console.log('fruits array :', fruits);
+console.log('fruis array length : ', fruits.length);
+
+console.log('mango : ', fruits[2]);
+fruits[1] = 'papaya'
+console.log('new array :', fruits);
+
+fruits[5] = 'mangoes'
+console.log('new array :', fruits);
+
+console.log('new array :', fruits[3]);
+console.log('new array :', fruits[4]);
