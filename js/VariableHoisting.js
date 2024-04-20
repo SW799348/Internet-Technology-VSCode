93a2e31ba8b50330aecacae65217689ba61e8420
sw799348 <sw799348@gmail.com>
2024-04-20 14:20:18 +0530

    variable hoisting done

--- js/VariableHoisting.js
+++ js/VariableHoisting.js
@@ -1,5 +1,21 @@
-var number = 100;
-var firstName = "samadhi";
+//1.using var
 
-console.log(number);
-console.log(firstName);
+console.log(number);    //not error
+
+var number = 100;
+
+//with var, variable declarations are hoisted to the top of their containing function
+//or global scope,which means you can access the variable before it's declared wihtout
+//causing an error, although its value will be undefined until it's assigned
+
+
+//2.using let
+
+console.log(firstName);   //  error (hoist wenawa but initialized wennah)
+let firstName = 'samadhi';
+
+
+//3.using const
+
+console.log(lastName);   //error (hoist wenawa but initialized wennah)
+const lastName='Perera'
